110a4dfcae4c6d2004f2af2f41b91737f085f461
bebe200 <tranlethanhminh@gmail.com>
2023-11-13 06:29:40 +0900

    Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+// Copyright Minh mechanics
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "UI/Widgets/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
