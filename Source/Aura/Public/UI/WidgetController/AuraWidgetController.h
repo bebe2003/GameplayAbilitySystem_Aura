ea93f73df3db5dacddf4e7c8fdc9de3dd1ef8704
bebe200 <tranlethanhminh@gmail.com>
2023-11-14 05:57:20 +0900

    Broadcasting Initial Values

--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystemComponent.h"
-#include "UObject/NoExportTypes.h"
 #include "AuraWidgetController.generated.h"
 
 class UAuraAttributeSet;
@@ -44,6 +43,8 @@ class AURA_API UAuraWidgetController : public UObject
 public:
 	UFUNCTION(BlueprintCallable)
 	void SetWidgetControllerParams(const FWidgetControllerParams& WCParams);
+
+	virtual void BroadcastInitialValues();
 protected:
 
 	UPROPERTY(BlueprintReadOnly, Category= "WidgetController")
