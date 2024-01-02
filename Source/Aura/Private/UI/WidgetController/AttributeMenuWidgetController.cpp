8c8d217de8f3706b6eae6c06f1f00388f5fbed06
bebe200 <tranlethanhminh@gmail.com>
2024-01-02 22:48:38 +0900

    Aura Ability System Blueprint Library

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Minh mechanics
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
